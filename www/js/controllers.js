b0b78df12ae9a3a72e47d79fe9d6548cd55e71a3
dhruvaldarji <dhruvaldarji@gmail.com>
2015-06-29 21:18:29 -0400

    Major Update: Remember login information. Lots of backend updates. Setup for future updates. ;) Began "Feeling It Up" for posts. Still has errors updating to DB once feels have been registered. Updates to "Commenting" and "Sharing".
    v.0.0.4

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,9 @@
 angular.module('starter.controllers', [])
-    .controller('AppCtrl', function ($scope, $state, $ionicModal, $timeout, Feels, Auth) {
+    .controller('AppCtrl', function ($scope, $state, $ionicModal, $localstorage, $timeout, Feels, Auth) {
         // With the new view caching in Ionic, Controllers are only called
         // when they are recreated or on app start, instead of every page change.
         // To listen for when this page is active (for example, to refresh data),
         // listen for the $ionicView.enter event:
-        // $scope.$on('$ionicView.enter', function(e) {
-        // });
 
         // Form data for the login modal
         $scope.loginData = {
@@ -31,7 +29,36 @@ angular.module('starter.controllers', [])
             msg: "",
             user: "",
             date: "",
+            feltBy: [],
+            comments: []
         };
+
+        var ref = new Firebase("https://datfeel.firebaseio.com");
+
+        $scope.$on('app.loggedOut', function(e) {
+            // Show the modal here
+        });
+
+        Auth.checkLogin();
+
+        $scope.$on('$ionicView.enter', function(e){
+            if(!$scope.loggedIn){
+                console.log("No one is logged in, checking local storage.");
+
+                var loginInfo = $localstorage.getObject('loginData').loginData;
+                console.log("Localstorage: ", loginInfo);
+                if($scope.loginData.remember === false){
+                    console.log("No login is saved, please login with email and password.");
+                    $scope.loginData = {};
+                }
+                else {
+                    $scope.loginData.username = loginInfo.username;
+                    $scope.loginData.password = loginInfo.password;
+                    $scope.loginData.remember = loginInfo.remember;
+                }
+                $scope.login();
+            }
+        });
 
         // Create the login modal that we will use later
         $ionicModal.fromTemplateUrl('templates/login.html', {
@@ -48,10 +75,10 @@ angular.module('starter.controllers', [])
         // Open the login modal
         $scope.login = function () {
             $scope.loginModal.show();
-            $scope.closeRegister();
+            if($scope.registerModal){
+                $scope.closeRegister();
+            }
         };
-
-        var ref = new Firebase("https://datfeel.firebaseio.com");
 
         // Perform the login action when the user submits the login form
         // Reference:     https://www.firebase.com/docs/web/guide/login/password.html
@@ -70,7 +97,6 @@ angular.module('starter.controllers', [])
                     console.log("Successfully logged in account with username:", authData.password.email);
                     $scope.currentUser = authData.password.email;
                     $scope.loggedIn = true;
-                    //$state.go('app.home');
                 }
             });
 
@@ -78,10 +104,21 @@ angular.module('starter.controllers', [])
             // code if using a login system
             $timeout(function () {
                 $scope.loading = false;
-                $scope.loginData = {
-                    username: "",
-                    password: ""
-                };
+                if($scope.loginData.remember){
+                    $localstorage.setObject('loginData', {
+                        loginData: $scope.loginData
+                    });
+                }
+                else{
+                    $scope.loginData = {
+                        username: "",
+                        password: "",
+                        remember: false
+                    };
+                    $localstorage.setObject('loginData', {
+                        loginData: $scope.loginData
+                    });
+                }
                 $scope.closeLogin();
             }, 1000);
         };
@@ -115,7 +152,9 @@ angular.module('starter.controllers', [])
         // Open the login modal
         $scope.register = function () {
             $scope.registerModal.show();
-            $scope.closeLogin();
+            if($scope.registerModal){
+                $scope.closeLogin();
+            }
         };
 
         // Perform the register action when the user submits the register form
@@ -140,31 +179,23 @@ angular.module('starter.controllers', [])
                     }
                 } else {
                     remember: "sessionOnly";
-                    console.log("Successfully created user account with uid:", userData.uid, "and email", userData.password.email);
+                    console.log("Successfully created user account with uid:", userData.uid);
+
+                    // Simulate a register delay. Remove this and replace with your login
+                    // code if using a login system
+                    $timeout(function () {
+                        $scope.closeRegister();
+                        $scope.login();
+                    }, 1000);
                 }
             });
 
-
-
-            // Simulate a login delay. Remove this and replace with your login
-            // code if using a login system
-            $timeout(function () {
-                $scope.loading = false;
-                $scope.registerData = {
-                    username: "",
-                    password: ""
-                };
-                $scope.closeLogin();
-            }, 1000);
+            $scope.loading = false;
+            $scope.registerData = {
+                username: "",
+                password: ""
+            };
         };
-
-
-        $scope.$on('$ionicView.enter', function(){
-            if(!$scope.loggedIn){
-                console.log("No one logged in, going to login view.");
-                $scope.login();
-            }
-        });
 
         // Create the login modal that we will use later
         $ionicModal.fromTemplateUrl('templates/create.html', {
@@ -193,9 +224,17 @@ angular.module('starter.controllers', [])
                     "user": $scope.currentUser,
                     "date": date.toDateString(),
                     "feel": PostFeelMessage,
+                    "feltBy": [$scope.currentUser],
+                    "comments": []
                 });
             }
-            $scope.feelsMessage.msg = "";
+            $scope.feelsMessage = {
+                msg: "",
+                user: "",
+                date: "",
+                feltBy: [],
+                comments: []
+            };
             $scope.closeCreate();
         }
 
@@ -203,8 +242,11 @@ angular.module('starter.controllers', [])
             var num = $scope.feels.length-id-1;
             var userDeleting = $scope.feels[num].user;
             if($scope.currentUser === userDeleting){
-                console.log("User: "+userDeleting+", is deleting DFW "+num+".");
-                $scope.feels.$remove(num);
+                var isConfirmed = confirm("Are you sure you want to delete DFW #"+num+".");
+                if (isConfirmed){
+                    console.log("User: "+userDeleting+", is deleting DFW "+num+".");
+                    $scope.feels.$remove(num);
+                }
             }
             else {
                 console.log("User: "+$scope.currentUser+" does not have permission to delete DFW "+num+".");
@@ -213,8 +255,29 @@ angular.module('starter.controllers', [])
 
         $scope.feelingItUp = function(id){
             var num = $scope.feels.length-id-1;
-            console.log("User: "+$scope.currentUser+", is feeling up DFW #"+num+".");
-            alert("User: "+$scope.currentUser+" is feeling up DFW #"+num+".");
+            //console.log("User: "+$scope.currentUser+", is feeling up DFW #"+num+".");
+            //alert("User: "+$scope.currentUser+" is feeling up DFW #"+num+".");
+            var add = true;
+            if(typeof($scope.feels[num].feltBy) !== 'undefined' && $scope.feels[num].feltBy.length){
+                var currentFeel = $scope.feels[num];
+                var numFelt = currentFeel.feltBy.length;
+                console.log("The feel has "+numFelt+" feels.")
+                for(var i = 0; i < numFelt; i++){
+                    if(currentFeel.feltBy[i] === $scope.currentUser) {
+                        //alert("User: "+$scope.currentUser+" has already felt up DFW #"+num+".");
+                        currentFeel.feltBy.splice(i,1,$scope.currentUser );
+                        add = false;
+                    }
+                }
+                if(add){
+                    $scope.feels[num].feltBy.push($scope.currentUser);
+                    //alert("User: "+$scope.currentUser+" is feeling up DFW #"+num+".");
+                }
+            }
+            else {
+                $scope.feels[num].feltBy = [$scope.currentUser];
+                //alert("User: "+$scope.currentUser+" is feeling up DFW #"+num+".");
+            }
         }
 
         $scope.commentFeel = function(id){
@@ -238,9 +301,83 @@ angular.module('starter.controllers', [])
     })
     .factory("Feels", function ($firebaseArray) {
         var itemsRef = new Firebase("https://datfeel.firebaseio.com/feels");
+        //console.log("Data", itemsRef);
+
+        function syncChanges(list, ref) {
+
+            ref.on('child_added', function _add(snap, prevChild) {
+                var data = snap.val();
+                data.$id = snap.key(); // assumes data is always an object
+                var pos = positionAfter(list, prevChild);
+                list.splice(pos, 0, data);
+            });
+
+            ref.on('child_removed', function _remove(snap) {
+                var i = positionFor(list, snap.key());
+                if( i > -1 ) {
+                    list.splice(i, 1);
+                }
+            });
+            ref.on('child_changed', function _change(snap) {
+                var i = positionFor(list, snap.key());
+                if( i > -1 ) {
+                    list[i] = snap.val();
+                    list[i].$id = snap.key(); // assumes data is always an object
+                }
+            });
+            ref.on('child_moved', function _move(snap, prevChild) {
+                var curPos = positionFor(list, snap.key());
+                if( curPos > -1 ) {
+                    var data = list.splice(curPos, 1)[0];
+                    var newPos = positionAfter(list, prevChild);
+                    list.splice(newPos, 0, data);
+                }
+            });
+        }
+
         return $firebaseArray(itemsRef);
     })
     .factory("Auth", function ($firebaseAuth) {
         var usersRef = new Firebase("https://datfeel.firebaseio.com/users");
         return $firebaseAuth(usersRef);
+    })
+    .factory('$localstorage', ['$window', function($window) {
+        return {
+            set: function(key, value) {
+                $window.localStorage[key] = value;
+            },
+            get: function(key, defaultValue) {
+                return $window.localStorage[key] || defaultValue;
+            },
+            setObject: function(key, value) {
+                $window.localStorage[key] = JSON.stringify(value);
+            },
+            getObject: function(key) {
+                return JSON.parse($window.localStorage[key] || '{}');
+            }
+        }}
+    ])
+    .factory('Auth', function($rootScope) {
+        return {
+            checkLogin: function() {
+                // Check if logged in and fire events
+                if(this.isLoggedIn()) {
+                    $rootScope.$broadcast('app.loggedIn');
+                } else {
+                    $rootScope.$broadcast('app.loggedOut');
+                }
+            },
+            isLoggedIn: function() {
+                // Check auth token here from localStorage
+            },
+            login: function(user, pass) {
+                // Do the login
+                // When done, trigger an event:
+                $rootScope.$broadcast('app.loggedIn');
+            },
+            logout: function(user, pass) {
+                // Same thing, log out user
+                $rootScope.$broadcast('app.loggedOut');
+            }
+        }
     });
