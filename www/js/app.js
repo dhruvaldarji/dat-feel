996ab5fb9c587142b4f2ce33732c509f5deaa9ac
dhruvaldarji <dhruvaldarji@gmail.com>
2015-07-02 20:27:57 -0400

    Added Ionic-Analytics Plugin

--- www/js/app.js
+++ www/js/app.js
@@ -4,10 +4,14 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'firebase', 'ngCordova', 'ionic.service.core', 'ionic.service.push','starter.controllers'])
+angular.module('starter', ['ionic', 'firebase', 'ngCordova', 'ionic.service.core', 'ionic.service.analytics', 'ionic.service.push', 'starter.controllers'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
+
+            // Add this inside your $ionicPlatform.ready function that is defined inside the run function:
+            $ionicAnalytics.register();
+
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -19,7 +23,7 @@ angular.module('starter', ['ionic', 'firebase', 'ngCordova', 'ionic.service.core
             }
         });
     })
-    .config(['$ionicAppProvider', function($ionicAppProvider) {
+    .config(['$ionicAppProvider', function ($ionicAppProvider) {
         // Identify app
         $ionicAppProvider.identify({
             // The App ID (from apps.ionic.io) for the server
