7c0dbb069820d36b8ae6128124dbfe913753b9a1
dhruvaldarji <dhruvaldarji@gmail.com>
2015-08-04 22:55:57 -0400

    Updated Ionic Service Core

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'firebase', 'ngCordova', 'monospaced.elastic', 'ionic.service.core',
+angular.module('starter', ['ionic','ionic.service.core', 'firebase', 'ngCordova', 'monospaced.elastic', 
     'ionic.service.analytics', 'ionic.service.push', 'starter.controllers'])
 
     .run(function ($ionicPlatform, $ionicAnalytics) {
@@ -34,7 +34,7 @@ angular.module('starter', ['ionic', 'firebase', 'ngCordova', 'monospaced.elastic
             // The GCM project number
             gcm_id: '94195404193',
             // Set the app to use development pushes
-            dev_push: true
+            dev_push: false
         });
     }])
     .config(function ($stateProvider, $urlRouterProvider) {
--- www/lib/ionic-service-core/ionic-core.js
+++ www/lib/ionic-service-core/ionic-core.js
@@ -1,5 +1,4 @@
 angular.module('ionic.service.core', [])
-
 /**
  * @private
  * Provides a safe interface to store objects in persistent memory
@@ -292,11 +291,12 @@ angular.module('ionic.service.core', [])
 */
 .factory('$ionicUser', [
   '$q',
+  '$ionicCoreSettings',
   '$timeout',
   '$http',
   'persistentStorage',
   '$ionicApp',
-function($q, $timeout, $http, persistentStorage, $ionicApp) {
+function($q, $ionicCoreSettings, $timeout, $http, persistentStorage, $ionicApp) {
       // User object we'll use to store all our user info
 
 
@@ -354,6 +354,12 @@ function($q, $timeout, $http, persistentStorage, $ionicApp) {
 
     _op: function(key, value, type) {
       var u = user.user_id;
+      var appId = '';
+      if ($ionicCoreSettings.get('app_id')) {
+        appId = $ionicCoreSettings.get('app_id')
+      } else {
+        appId = $ionicApp.getId();
+      }
       if(!u) {
         throw new Error("Please call identify with a user_id before calling push");
       }
@@ -361,7 +367,7 @@ function($q, $timeout, $http, persistentStorage, $ionicApp) {
       o['user_id'] = u;
       o[key] = value;
 
-      return $http.post($ionicApp.getApiUrl() + '/api/v1/app/' + $ionicApp.getId() + '/users/' + type, o);
+      return $http.post($ionicApp.getApiUrl() + '/api/v1/app/' + appId + '/users/' + type, o);
     },
     /**
      * Push the given value into the array field identified by the key.
@@ -398,6 +404,12 @@ function($q, $timeout, $http, persistentStorage, $ionicApp) {
       return generateGuid();
     },
     identify: function(userData) {
+      var appId = '';
+      if ($ionicCoreSettings.get('app_id')) {
+        appId = $ionicCoreSettings.get('app_id')
+      } else {
+        appId = $ionicApp.getId();
+      }
       if (!userData.user_id) {
         var msg = 'You must supply a unique user_id field.';
         throw new Error(msg)
@@ -409,9 +421,15 @@ function($q, $timeout, $http, persistentStorage, $ionicApp) {
       // Write the user object to our local storage
       persistentStorage.storeObject(storageKeyName, user);
 
-      return $http.post($ionicApp.getApiUrl() + '/api/v1/app/' + $ionicApp.getId() + '/users/identify', userData);
+      return $http.post($ionicApp.getApiUrl() + '/api/v1/app/' + appId + '/users/identify', userData);
     },
     identifyAnonymous: function() {
+      var appId = '';
+      if ($ionicCoreSettings.get('app_id')) {
+        appId = $ionicCoreSettings.get('app_id')
+      } else {
+        appId = $ionicApp.getId();
+      }
       userData = {};
       userData['user_id'] = generateGuid();
       userData['isAnonymous'] = true;
@@ -422,13 +440,27 @@ function($q, $timeout, $http, persistentStorage, $ionicApp) {
       // Write the user object to our local storage
       persistentStorage.storeObject(storageKeyName, user);
 
-      return $http.post($ionicApp.getApiUrl() + '/api/v1/app/' + $ionicApp.getId() + '/users/identify', userData);
+      return $http.post($ionicApp.getApiUrl() + '/api/v1/app/' + appId + '/users/identify', userData);
     },
     get: function() {
       return user;
     }
   }
 }])
+
+// Auto-generated configuration factory
+.factory('$ionicCoreSettings', function() {
+  var settings = {"app_id":"45ec6dc0","api_key":"23388e70ab4483be754cf84fe8220d9e49baa88c48f5dad4","dev_push":false,"gcm_id":"94195404193"};
+  return {
+    get: function(setting) {
+      if (settings[setting]) {
+        return settings[setting];
+      }
+      return null;
+    }
+  }
+})
+// Auto-generated configuration factory
 
 .run(['$ionicApp', function($ionicApp) {
   console.log('Ionic Core: init');
